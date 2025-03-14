67630376adaeffcce1a444d21c64cccce849c2e4
cl-davii <davifsc03@gmail.com>
2025-03-14 16:51:41 -0300

    Principais metodos para manipulacao de entidades no banco de dados

--- src/main/java/com/davi/timetable/repositories/SubjectRepository.java
+++ src/main/java/com/davi/timetable/repositories/SubjectRepository.java
@@ -1,4 +1,15 @@
 package com.davi.timetable.repositories;
 
+import com.davi.timetable.entities.Subject;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface SubjectRepository {
+    Subject save(Subject subject);
+    Subject update(Subject subject);
+    Subject findById(UUID subjectId);
+    Subject findByCodeOrName(String subjectCode, String subjectName);
+    List<Subject> findAll();
+    void delete(UUID subjectId);
 }
