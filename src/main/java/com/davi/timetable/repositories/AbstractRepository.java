54c23584a5487223e6faeb05da3b294460aa05c6
cl-davii <davifsc03@gmail.com>
2025-03-14 16:50:40 -0300

    Classe abstrata criada para chamada de metodos de manipulacao de entidades

--- src/main/java/com/davi/timetable/repositories/AbstractRepository.java
+++ src/main/java/com/davi/timetable/repositories/AbstractRepository.java
@@ -1,4 +1,42 @@
 package com.davi.timetable.repositories;
 
-public class AbstractRepository {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+public abstract class AbstractRepository<T, PK extends Serializable> {
+
+    @SuppressWarnings("unchecked")
+    private final Class<T> entityClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public T save(T entity) {
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    public T update(T entity) {
+        entityManager.merge(entity);
+        return entity;
+    }
+
+    public void delete(UUID id) {
+        entityManager.remove(entityManager.getReference(entityClass, id));
+    }
+
+    public T findById(UUID id) {
+        return entityManager.find(entityClass, id);
+    }
+
+    public List<T> findAll() {
+        return entityManager.createQuery("FROM " + entityClass.getSimpleName(), entityClass).getResultList();
+    }
 }
